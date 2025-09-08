e622d4bc2743b0c7683d19b797392d82c16afce6
mantashasheikh <mantashasheikh95844@gmail.com>
2025-09-08 21:31:33 +0530

    movie ticket booking form

--- form.js
+++ form.js
@@ -1,3 +1,28 @@
+let getdata=async()=>{
+    let api="http://localhost:3000/MovieTicketBooking"
+    let res = await fetch(api, {method:"GET"})
+    let data = await res.json()
+
+
+    let show = document.querySelector("#show")
+    data.map((e)=>{
+    show.innerHTML +=`
+    <tr>
+        <td>${e.title}</td>
+        <td>${e.date}</td>
+        <td>${e.time}</td>
+        <td>${e.ticket}</td>
+        <td>${e.price}</td>
+        <td>${e.email}</td>
+        <td>${e.fname}</td>
+        <td>${e.lname}</td>
+    </tr>
+    `
+})
+}
+
+
+
 
 let PostData=()=>{
 
@@ -5,7 +30,7 @@ let PostData=()=>{
    let Date =document.querySelector("#date").value
    let Time =document.querySelector("#time").value
    let Ticket =document.querySelector("#ticket").value
-   let Tickets =document.querySelector("#tickets").value
+   let Price=document.querySelector("#price").value
    let Email =document.querySelector("#email").value
    let Fname =document.querySelector("#fname").value
    let Lname=document.querySelector("#lname").value
@@ -23,14 +48,15 @@ let PostData=()=>{
 
         body: JSON.stringify( 
           {
-              name:Fname,
-              BookingDate:Date,
+              name:Title,
+              Booking_Date:Date,
               time:Time,
-              adult:Ticket,
-              children:Tickets,
+              No_of_tickets:Ticket,
+              Total_price:Price,
               email:Email,
-              firstnameFname,
-              lastname:Lname
+              first_name:Fname,
+              last_name:Lname
+
 
           }
          )
