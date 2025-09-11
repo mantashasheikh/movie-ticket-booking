7ea3fd60be38cb12f6b38a24038ec729bc9dafca
mantashasheikh <mantashasheikh95844@gmail.com>
2025-09-11 09:35:59 +0530

    javascript

--- form.js
+++ form.js
@@ -1,75 +1,195 @@
-let submit=async()=>{
+ let getdata=async()=>{
+
     let api="http://localhost:3000/MovieTicketBooking"
-    let res = await fetch(api, {method:"GET"})
-    let data = await res.json()
+
+   let res=await fetch(api, {method:"GET"} )
+
+   let data=await res.json()
+
+   console.log(data);
+
+   let Show=document.querySelector("#show")
+    
+    data.map( (e)=>{
+
+        Show.innerHTML+= ` 
+        <tr>
+           <td> ${e.title} </td>
+           <td> ${e.date} </td>
+           <td> ${e.time} </td>
+           <td> ${e.ticket} </td>
+           <td> ${e.ticket*500} </td>
+           <td> ${e.email} </td>
+           <td> ${e.fname} </td>
+           <td> ${e.lname} </td>
+           <td onclick="Del('${e.id}')"> Cancel </td> 
+           <td onclick="edit('${e.id}')"> Update </td> 
+        </tr>
+        
+        `
+    })
 
 
-    let show = document.querySelector("#show")
-    data.map((e)=>{
-    show.innerHTML +=`
-    <tr>
-        <td>${e.title}</td>
-        <td>${e.date}</td>
-        <td>${e.time}</td>
-        <td>${e.ticket}</td>
-        <td>${e.price}</td>
-        <td>${e.email}</td>
-        <td>${e.fname}</td>
-        <td>${e.lname}</td>
-    </tr>
-    `
-})
-}
- submit()
+
+ }
+
+ let Del=(id)=>{
+
+        let api=`http://localhost:3000/MovieTicketBooking/${id}`
+        fetch(api, {method:"DELETE"} )    
+
+ }
 
 
 
+ let PostData=()=>{
 
-// let submit=()=>{
-
-//    let Title =document.querySelector("#title").value
-//    let Date =document.querySelector("#date").value
-//    let Time =document.querySelector("#time").value
-//    let Ticket =document.querySelector("#ticket").value
-//    let Price=document.querySelector("#price").value
-//    let Email =document.querySelector("#email").value
-//    let Fname =document.querySelector("#fname").value
-//    let Lname=document.querySelector("#lname").value
+   let Name =document.querySelector("#name").value
+   let Age =document.querySelector("#age").value
+   let Aadhar =document.querySelector("#aadhar").value
+   let CheckIn =document.querySelector("#checkin").value
+   let CheckOut =document.querySelector("#checkout").value
+   let City =document.querySelector("#city").value
+   let People =document.querySelector("#people").value
 
     
-//    let api="http://localhost:3000/movieTicketBooking"
+   let api="http://localhost:3000/hotel"
 
-//    fetch(api, {  
+   fetch(api, {  
 
-//         method:"POST",
+        method:"POST",
          
-//         header:{
-//            "Content-type":"application/json"
-//         },
+        header:{
+           "Content-type":"application/json"
+        },
 
-//         body: JSON.stringify( 
-//           {
-//               name:Title,
-//               Booking_Date:Date,
-//               time:Time,
-//               No_of_tickets:Ticket,
-//               Total_price:Price,
-//               email:Email,
-//               first_name:Fname,
-//               last_name:Lname
+        body: JSON.stringify( 
+          {
+              name:Name,
+              age:Age,
+              aadhar:Aadhar,
+              checkIn:CheckIn,
+              checkOut:CheckOut,
+              city:City,
+              people:People,
+              price:500
 
-
-//           }
-//          )
-
-
-//      })
+          }
+         )
+    })
 
 
-//      location.href="dataTable.html"
-//      return false
+     location.href="CRUD.html"
+     return false
 
-// }
+}
+
+
+
+let edit=async(id)=>{
+
+         let api= http://localhost:3000/hotel/${id}
+         
+        let res=await fetch(api,{method:"GET"}) 
+
+        let data=await res.json()
+        
+        console.log(data);
+        
+
+
+   document.querySelector("#formshow").innerHTML= `
+   
+           
+        Enter Name <input type="text" id="upname" value="${data.name}"> <br> <br>
+
+        
+        Enter age <input type="text" id="upage" value="${data.age}" > <br> <br>
+
+       
+        Enter Adhar <input type="text" id="upaadhar" value="${data.aadhar}" > <br> <br>
+
+       
+        Enter Check-In <input type="date" id="upcheckin" value="${data.checkIn}" > <br> <br>
+
+        <p id="errcpass"></p>
+        Enter Check-Out <input type="date" id="upcheckout" value="${data.checkOut}" > <br> <br>
+        
+        Select City  <select id="upcity">
+          <option value="Bhopal">Bhopal</option>
+          <option value="Indore">Indore</option>
+          <option value="Ujjain">Ujjain</option>
+          <option value="Kalapani">Kalapani</option>
+           <option value="Pakistan">Pakistan</option>
+
+
+        </select> <br> <br>
+
+        Enter people <input type="text" id="uppeople" value="${data.people}"> <br> <br>
+
+
+        <input type="submit" value="Update-Now" onclick="return UpdateData('${data.id}')">
+   
+   
+   `
+
+}
+
+
+
+let UpdateData=(id)=>{
+
+    let Name =document.querySelector("#upname").value
+   let Age =document.querySelector("#upage").value
+   let Aadhar =document.querySelector("#upaadhar").value
+   let CheckIn =document.querySelector("#upcheckin").value
+   let CheckOut =document.querySelector("#upcheckout").value
+   let City =document.querySelector("#upcity").value
+   let People =document.querySelector("#uppeople").value
+
+    
+   let api=http://localhost:3000/hotel/${id}
+
+   fetch(api, {  
+
+        method:"PUT",
+         
+        header:{
+           "Content-type":"application/json"
+        },
+
+        body: JSON.stringify( 
+          {
+              name:Name,
+              age:Age,
+              aadhar:Aadhar,
+              checkIn:CheckIn,
+              checkOut:CheckOut,
+              city:City,
+              people:People,
+              price:500
+          }
+         )
+
+
+     })
+
+
+     location.href="CRUD.html"
+     return false
+
+
+}
+
+
+
+
+
+  
+
+
+
+
 
 
 
